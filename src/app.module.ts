722f1527278497a3b672d842e90e10e9dba72bdb
FranklinParker <fparker@deliveryman.com>
2021-04-02 12:24:17 -0400

    logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
+import { LoggerMiddleware } from './shared/middleware/logger.middleware';
+import { ContactsController } from './contacts/contacts.controller';
 
 @Module({
   imports: [ContactsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes(ContactsController);
+  }
+}
---
+++ src/shared/middleware/logger.middleware.ts
@@ -0,0 +1,12 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request } from 'express';
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  logger = new Logger('loggerMiddleWare');
+  use(req: Request, res: any, next: () => void) {
+    this.logger.debug('Req headers: ' + req.headers);
+    this.logger.debug('Req Method: ' + req.method);
+    this.logger.debug('req url' + req.originalUrl);
+    next();
+  }
+}
