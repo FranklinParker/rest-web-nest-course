bd72f9c592ec4360a7c00355383c7738e375f999
FranklinParker <fparker@deliveryman.com>
2021-04-02 13:10:20 -0400

    use helmet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,24 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
 import { LoggerMiddleware } from './shared/middleware/logger.middleware';
+import * as helmet from 'helmet';
 
 @Module({
   imports: [ContactsModule],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): any {
     consumer
-      .apply(LoggerMiddleware)
-      .forRoutes(
-      { path: 'contacts*', method: RequestMethod.GET },
-
-      { path: 'contacts', method: RequestMethod.POST },
-    );
+      .apply(helmet(), LoggerMiddleware)
+      .forRoutes({ path: 'contacts*', method: RequestMethod.ALL });
   }
 }
--- src/shared/middleware/logger.middleware.ts
+++ src/shared/middleware/logger.middleware.ts
@@ -4,7 +4,7 @@ import { Request } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
   logger = new Logger('loggerMiddleWare');
   use(req: Request, res: any, next: () => void) {
-    this.logger.debug('Req headers: ' + req.headers);
+    this.logger.debug('Req headers: ' + JSON.stringify(req.headers));
     this.logger.debug('Req Method: ' + req.method);
     this.logger.debug('req url' + req.originalUrl);
     this.logger.debug('body:' + JSON.stringify(req.body));
